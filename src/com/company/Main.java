ec54b580a37858403efdf44b7159c756e4dd98b8
ElFerde <ferdinand.ronngren@gmail.com>
2020-08-12 14:09:36 +0200

    Fix scrollsak

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,7 +12,6 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -36,8 +35,8 @@ public class Main extends Application {
     static Button run = new Button("RUN");
     static Button grid = new Button("GRID");
     static FractranProgram program;
-    static ScrollPane displayFractions = new ScrollPane();
     static ScrollPane displayState = new ScrollPane();
+    static ScrollPane displayFractions = new ScrollPane();
     static TextField tstate = new TextField();
     static Canvas fractions;
     static AnchorPane root;
@@ -132,7 +131,8 @@ public class Main extends Application {
         FractranGenerator.generateProgram(sizex, sizey);
         program = new FractranProgram();
         root = new AnchorPane();
-        if (displayFractions())
+        boolean isFractions = displayFractions();
+        if (isFractions)
         {
             windowheight += 110;
         }
@@ -147,6 +147,7 @@ public class Main extends Application {
         root.getChildren().add(run);
         root.getChildren().add(grid);
         root.getChildren().add(displayState);
+
         stage.setResizable(false);
     
         canvas.setOnMousePressed(clickedBoard);
@@ -154,19 +155,29 @@ public class Main extends Application {
         canvas.setOnMouseReleased(releasedBoard);
         run.setOnAction(clickRun);
         grid.setOnAction(clickGrid);
+        if (isFractions) {
+            root.getChildren().add(displayFractions);
+            AnchorPane.setTopAnchor(displayFractions, (double) (10 + sizey * 20 + 50));
+            AnchorPane.setLeftAnchor(displayFractions, (double) 10);
+            displayFractions.setContent(fractions);
+            displayFractions.setPrefSize(sizex * 20, 100);
+            displayFractions.setHbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+            displayFractions.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+        }
 
         AnchorPane.setTopAnchor(run, (double) (sizey * 20 + 20));
         AnchorPane.setLeftAnchor(run, 10.0);
         AnchorPane.setTopAnchor(displayState, (double) (sizey * 20 + 20));
         AnchorPane.setLeftAnchor(displayState, 70.0);
-        
+
+
         tstate.setText("2");
         tstate.setMinSize(sizex * 20 - 60, 20);
         displayState.setContent(tstate);
         displayState.setPrefSize(sizex * 20 - 60, 20);
         displayState.setHbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
         displayState.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        
+
         
         gc.setFill(Color.ANTIQUEWHITE);
         gc.fillRect(0, 0, sizex * 20 + 20, windowheight);
@@ -224,13 +235,14 @@ public class Main extends Application {
         
         if (pos[1] < 4096 - rowheight)
         {
+            /*root.getChildren().add(displayFractions);
             displayFractions.setContent(fractions);
             displayFractions.setPrefSize(sizex*20, 100);
-            displayState.setHbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-            displayState.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+            displayFractions.setHbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+            displayFractions.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
             AnchorPane.setTopAnchor(displayFractions, (double) (10 + sizey * 20 + 50));
-            AnchorPane.setLeftAnchor(displayFractions, (double) 10);
-            root.getChildren().add(displayFractions);
+            AnchorPane.setLeftAnchor(displayFractions, (double) 10);*/
+
             return true;
         }
         return false;
